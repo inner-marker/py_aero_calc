cd4058bf2db68dc857716f1412394cc92bf8c46b
Stuart Russell <stuart.s.russell@icloud.com>
2023-11-23 14:22:37 -0600

    mod user io

--- e6b_funcs.py
+++ e6b_funcs.py
@@ -2,20 +2,29 @@
 # Calculation functions
 ############################################################
 
-def calc_groundspeed_knots (distance_nm, hours):
+def calc_groundspeed_knots (distance_nm, time, time_unit = "hours"):
     """calculate grounud speed in knots
 
     Args:
         distance_nm (float): distance in nautical miles
         hours (float): hours to travel the distance
+        time_unit (Str): Time unit. "hours" (default), "minutes"
 
     Returns:
         float: ground speed in knots
     """ 
     
     groundspeed_knots = 0
+    time_hours = 0
+    
+    # process the time variable
+    if time_unit == "minutes":
+        time_hours = time / 60
+    else:
+        time_hours = time
+        
     try:
-        groundspeed_knots = distance_nm / hours
+        groundspeed_knots = distance_nm / time_hours
     except:
         print('possible divide by zero error')
     return groundspeed_knots
@@ -30,15 +39,16 @@ def userio_groundspeed_knots ():
     
     # Create the variables
     user_distance_nautical_miles = 0
-    user_hours = 0
+    user_time = 0
     groundspeed_knots = 0
+    user_time_unit = "hours"
     
     # Get the values
     user_distance_nautical_miles = float(input('Distance (nm):    '))
-    user_hours =                   float(input('Hours (decimal):  '))
-    
+    user_time =                    float(input('Time (decimal):  '))
+    user_time_unit = input("Time Unit ('hours', 'minutes'): ")
     # Calculat the result
-    groundspeed_knots = calc_groundspeed_knots(user_distance_nautical_miles,user_hours)
+    groundspeed_knots = calc_groundspeed_knots(user_distance_nautical_miles,user_time, time_unit=user_time_unit)
     
     # Print the result
     print("Ground Speed (knots): ", "{:.1f}".format(groundspeed_knots))
